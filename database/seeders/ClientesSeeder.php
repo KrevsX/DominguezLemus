2fc00e3458a921aa0d6edcbd9581ee6bbdb05db7
Kevin Alas <elmrochino.yo@gmail.com>
2023-05-07 16:39:51 -0600

    Amend..//

---
+++ database/seeders/ClientesSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClientesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(App\Models\Cliente::class, 12)->create();
+        
+    }
+}
