343b8fde59c48b36e774a83e2a803f8a8d1ff7a4
Kevin Alas <elmrochino.yo@gmail.com>
2023-05-10 19:20:25 -0600

    amend*//

--- database/seeders/ArticulosSeeder.php
+++ database/seeders/ArticulosSeeder.php
@@ -14,7 +14,22 @@ class ArticulosSeeder extends Seeder
      */
     public function run()
     {
-        //
-        factory(App\Models\Articulo::class, 12)->create();
+        $articulos = [
+            //Ingresar datos cualquiera para cargar a la base de datos...
+            ['nombre' => 'Samsung S23', 'descripcion' => 'Telefono Gama Alta', 'cantInventario' => '20', 'precio' => '999.99'],        /*1*/ 
+            ['nombre' => 'Samsung S22', 'descripcion' => 'Telefono Gama Alta', 'cantInventario' => '19', 'precio' => '850.99'],     /*2*/
+            ['nombre' => 'Iphone 14 Pro Max', 'descripcion' => 'Telefono Gama Alta', 'cantInventario' => '26', 'precio' => '1599.99'], /*3*/
+            ['nombre' => 'Iphone 13 Pro Max', 'descripcion' => 'Telefono Gama Alta', 'cantInventario' => '23', 'precio' => '1299.99'], /*4*/
+            ['nombre' => 'Macbook Air', 'descripcion' => 'Laptop Gama Alta', 'cantInventario' => '20', 'precio' => '1500.00'],   /*5*/
+            ['nombre' => 'Macbook Pro', 'descripcion' => 'Laptop Gama Alta', 'cantInventario' => '15', 'precio' => '1999.99'],        /*6*/
+            ['nombre' => 'iMac 24Inch', 'descripcion' => 'Desktop Gama Alta', 'cantInventario' => '9', 'precio' => '1399.99'],     /*7*/
+            ['nombre' => 'iMac 22Inch', 'descripcion' => 'Desktop Gama Alta', 'cantInventario' => '5', 'precio' => '1100.99'],      /*8*/
+            ['nombre' => 'Samsung Smart Watch Pro', 'descripcion' => 'Accesorio Gama Alta', 'cantInventario' => '7', 'precio' => '499.99'],     /*9*/
+            ['nombre' => 'Samsung Smart Watch G3', 'descripcion' => 'Accesorio Gama Alta', 'cantInventario' => '12', 'precio' => '349.99'],   /*10*/
+            ['nombre' => 'Apple Watch S8', 'descripcion' => 'Accesorio Gama Alta', 'cantInventario' => '19', 'precio' => '999.99'], /*11*/
+            ['nombre' => 'Apple Watch S7', 'descripcion' => 'Accesorio Gama Alta', 'cantInventario' => '9', 'precio' => '699.99'], /*12*/
+        ];
+    
+        DB::table('articulos')->insert($articulos);
     }
 }
--- database/seeders/PedidosSeeder.php
+++ database/seeders/PedidosSeeder.php
@@ -14,7 +14,22 @@ class PedidosSeeder extends Seeder
      */
     public function run()
     {
-        //
-        factory(App\Models\Pedido::class, 12)->create();
+        $pedidos = [
+            //Ingresar datos cualquiera para cargar a la base de datos...
+            ['fechaPedido' => '2023-05-06', 'fechaEntrega' => '2023-05-07','Observaciones'=> 'Pendiente Entrega'],        /*1*/ 
+            ['fechaPedido' => '2023-05-06', 'fechaEntrega' => '2023-05-08','Observaciones'=> 'Pendiente Entrega'],     /*2*/
+            ['fechaPedido' => '2023-05-04', 'fechaEntrega' => '2023-05-09','Observaciones'=> 'Pendiente Entrega'],      /*3*/
+            ['fechaPedido' => '2023-05-03', 'fechaEntrega' => '2023-05-08','Observaciones'=> 'Pendiente Entrega'],    /*4*/
+            ['fechaPedido' => '2023-05-01', 'fechaEntrega' => '2023-05-07','Observaciones'=> 'Pendiente Entrega'],     /*5*/
+            ['fechaPedido' => '2023-05-02', 'fechaEntrega' => '2023-05-09','Observaciones'=> 'Pendiente Entrega'],      /*6*/
+            ['fechaPedido' => '2023-05-02', 'fechaEntrega' => '2023-05-08','Observaciones'=> 'Pendiente Entrega'],      /*7*/
+            ['fechaPedido' => '2023-05-05', 'fechaEntrega' => '2023-05-07','Observaciones'=> 'Pendiente Entrega'],        /*8*/
+            ['fechaPedido' => '2023-05-02', 'fechaEntrega' => '2023-05-10','Observaciones'=> 'Pendiente Entrega'],       /*9*/
+            ['fechaPedido' => '2023-05-02', 'fechaEntrega' => '2023-05-11','Observaciones'=> 'Pendiente Entrega'],    /*10*/
+            ['fechaPedido' => '2023-05-05', 'fechaEntrega' => '2023-05-10','Observaciones'=> 'Pendiente Entrega'],   /*11*/
+            ['fechaPedido' => '2023-05-06', 'fechaEntrega' => '2023-05-09','Observaciones'=> 'Pendiente Entrega'], /*12*/
+        ];
+    
+        DB::table('pedidos')->insert($pedidos);
     }
 }
