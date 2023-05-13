aa3b87aefcc35b238acab90a555cace81fc40f0c
Kevin Alas <elmrochino.yo@gmail.com>
2023-05-12 20:27:43 -0600

    seeders/mod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(ClientesSeeder::class);
+        $this->call(ArticulosSeeder::class);
+        $this->call(PedidosSeeder::class);
+        $this->call(DetallePedidosSeeder::class);
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
